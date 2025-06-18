8b8164bf46d59bdc8e77fbf061f3219177816667
Arthur Toppenberg <Arthur.Toppenberg@gmail.com>
2025-06-19 00:29:15 +0200

    Add Sign Out button to Home component for user session management

--- apps/nextjs/src/app/(root)/(application)/page.tsx
+++ apps/nextjs/src/app/(root)/(application)/page.tsx
@@ -1,3 +1,16 @@
+import { Button } from "@gym/ui/components/button";
+import { signOut } from "next-auth/react";
+
 export default async function Home() {
-  return <div>HOME</div>;
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          signOut;
+        }}
+      >
+        Sign Out
+      </Button>
+    </div>
+  );
 }
