bc569b713285b978057f5d8b9771dd1a90b2cd00
Arthur Toppenberg <Arthur.Toppenberg@gmail.com>
2025-06-22 15:37:31 +0200

    Add BLOB_READ_WRITE_TOKEN to environment configuration
    - Updated turbo.json to include BLOB_READ_WRITE_TOKEN in the environment variables.
    - Enhanced env.ts to define BLOB_READ_WRITE_TOKEN as a required string environment variable.

--- packages/env/src/env.ts
+++ packages/env/src/env.ts
@@ -16,6 +16,7 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    BLOB_READ_WRITE_TOKEN: z.string(),
   },
 
   /**
@@ -37,6 +38,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
+    BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
