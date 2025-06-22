282933605b20896f1d200562ed7b2c10eb308885
Arthur Toppenberg <Arthur.Toppenberg@gmail.com>
2025-06-22 20:28:28 +0200

    Update Providers component to include client directive and adjust the order of SessionProvider and TRPCReactProvider for improved structure and readability.

--- apps/nextjs/src/app/providers.tsx
+++ apps/nextjs/src/app/providers.tsx
@@ -1,15 +1,17 @@
+"use client";
+
 import { TRPCReactProvider } from "@gym/trpc/react";
 import { Toaster } from "@gym/ui/components/sonner";
 import { SessionProvider } from "next-auth/react";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SessionProvider>
-      <TRPCReactProvider>
+    <TRPCReactProvider>
+      <SessionProvider>
         {children}
         <Toaster />
-      </TRPCReactProvider>
-    </SessionProvider>
+      </SessionProvider>
+    </TRPCReactProvider>
   );
 };
 
