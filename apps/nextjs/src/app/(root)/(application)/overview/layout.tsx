1fcce3701205a104d073f0c01bb2034f53b949bf
Arthur Toppenberg <Arthur.Toppenberg@gmail.com>
2025-06-22 22:00:05 +0200

    Remove Overview components and related files for a cleaner application structure.

--- apps/nextjs/src/app/(root)/(application)/overview/layout.tsx
+++
@@ -1,7 +0,0 @@
-const OverviewLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex h-full w-full flex-col px-6 pt-6">{children}</div>
-  );
-};
-
-export default OverviewLayout;
