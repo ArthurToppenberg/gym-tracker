474a126857a27e7a6669dfa238710ad22a81dfac
Arthur Toppenberg <Arthur.Toppenberg@gmail.com>
2025-06-22 22:02:09 +0200

    Remove Exercise components and related files to streamline the exercises feature.

--- apps/nextjs/src/app/(root)/(application)/exercises/exerciseCreate/types.ts
+++
@@ -1,5 +0,0 @@
-import type { inferRouterOutputs } from "@trpc/server";
-import type { AppRouter } from "@gym/trpc/server";
-
-export type ExerciseVariation =
-  inferRouterOutputs<AppRouter>["exercises"]["getExerciseVariations"]["variations"][number];
--- apps/nextjs/src/app/(root)/(application)/exercises/layout.tsx
+++
@@ -1,9 +0,0 @@
-const ExercisesLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex h-full w-full flex-col items-center gap-4 px-2 pt-2 pb-20 lg:px-[25%] lg:pt-6 lg:pb-0">
-      {children}
-    </div>
-  );
-};
-
-export default ExercisesLayout;
